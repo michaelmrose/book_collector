81cb04328ae0cac995699ed1a268e74b739c3599
Michael Rose <michael@rosenetwork.net>
2023-08-12 00:24:09 -0700

    mockup index functionality home and about

--- collector/collection/views.py
+++ collector/collection/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
 
+bookList = [
+    {"title": "Dune", "author": "Frank Herbert", "published": "1965"},
+    {"title": "Starfish", "author": "Peter Watts", "published": "1999"},
+    {"title": "The Gone World", "author": "Tom Sweterlitsch", "published": 2018},
+]
+
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def books_index(request):
+    return render(request, "books/index.html", {"books": bookList})
+
+
+def about(request):
+    return render(request, "about.html")
